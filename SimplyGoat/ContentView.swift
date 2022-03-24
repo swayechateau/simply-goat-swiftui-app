e74bcc60ebfa4ebfeeb62299462e5677fab01c69
Swaye Chateau <swaye@kitechsoftware.com>
2022-03-24 06:37:30 +0000

    Initial Commit

---
+++ SimplyGoat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimplyGoat
+//
+//  Created by Swaye Chateau on 24/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimplyGoat/SimplyGoatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyGoatApp.swift
+//  SimplyGoat
+//
+//  Created by Swaye Chateau on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimplyGoatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
